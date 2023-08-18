c29426a5a5cc85678894f5c4504e4a4958c70d09
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-18 14:25:39 -0600

    adds drop shadow to pokemon

--- src/components/Controls/Style.js
+++ src/components/Controls/Style.js
@@ -7,9 +7,6 @@ export default function Style() {
     <select onChange={(event) => setArtStyle(event.target.value)}>
       <option value="default">Default</option>
       <option value="shiny">Shiny</option>
-      {/* <option>Official</option>
-      <option>Dream World</option>
-      <option>Home</option> */}
     </select>
   );
 }
